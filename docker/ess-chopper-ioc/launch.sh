7370dc2892d96e4f62b32cd2919262546e7bb621
Matthew D Jones <matthew.d.jones@tessella.com>
2017-11-21 15:27:34 +0000

    Re #7 set forwarder config using environment variables

--- docker/ess-chopper-ioc/launch.sh
+++ docker/ess-chopper-ioc/launch.sh
@@ -3,5 +3,13 @@
 # Start the chopper
 lewis chopper -p "epic: {prefix: 'SIM:'}"
 
-# Publish on the forwarder config channel that we want this PV forwarded
-kafkacat -b ${KAFKA_BROKER_NAME:=localhost}-t ${FORWARDER_CONFIG_TOPIC:=TEST_forwarderConfig} -P forwarder_config.json
+# Publish on the forwarder config channel that we want the speed and phase PVs forwarded
+echo "{\"cmd\":\"add\",\"streams\":[{\"channel\":\"SIM:SIM:Spd-RB\",\"channel_provider_type\":\"ca\",\"converter\":{\"schema\":\"f142\",\"topic\":\"${FORWARDER_OUTPUT_TOPIC}\"}},"\
+"{\"channel\":\"SIM:SIM:Phs-RB\",\"channel_provider_type\":\"ca\",\"converter\":{\"schema\":\"f142\",\"topic\":\"${FORWARDER_OUTPUT_TOPIC}\"}}]}"|\
+kafkacat -P -b ${KAFKA_BROKER_NAME:=localhost}-t ${FORWARDER_CONFIG_TOPIC}
+
+# Move the chopper to a specified speed and phase
+caput SIM:CmdS init
+caput SIM:Spd 100.0
+caput SIM:Phs 23.0
+caput SIM:CmdS start

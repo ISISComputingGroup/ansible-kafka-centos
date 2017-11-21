1c4c1f5f1a5fa50a04eccc11d1ab9b2331f38279
Matthew D Jones <matthew.d.jones@tessella.com>
2017-11-21 16:35:10 +0000

    Re #7 fix entrypoint so that launch script is run

--- docker/ess-chopper-ioc/launch.sh
+++ docker/ess-chopper-ioc/launch.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+KAFKA_BROKER_NAME=${KAFKA_BROKER_NAME:=localhost}
+
+# Wait for Kafka broker to be up
+kafkacat -b kafka -L
+OUT=$?
+i="0"
+while [ $OUT -ne 0 -a  $i -ne 5  ]; do
+   echo "Waiting for Kafka to be ready"
+   kafkacat -b ${KAFKA_BROKER_NAME} -L
+   OUT=$?
+   let i=$i+1
+   echo $i
+done
+if [ $i -eq 5 ]
+then
+   echo "Kafka broker not accessible at producer launch"
+   exit 1
+fi
+
 # Start the chopper
-lewis chopper -p "epic: {prefix: 'SIM:'}"
+lewis chopper -p "epics: {prefix: '${CHANNEL_PREFIX:=SIM}:'}"
 
 # Publish on the forwarder config channel that we want the speed and phase PVs forwarded
 echo "{\"cmd\":\"add\",\"streams\":[{\"channel\":\"SIM:SIM:Spd-RB\",\"channel_provider_type\":\"ca\",\"converter\":{\"schema\":\"f142\",\"topic\":\"${FORWARDER_OUTPUT_TOPIC}\"}},"\
 "{\"channel\":\"SIM:SIM:Phs-RB\",\"channel_provider_type\":\"ca\",\"converter\":{\"schema\":\"f142\",\"topic\":\"${FORWARDER_OUTPUT_TOPIC}\"}}]}"|\
-kafkacat -P -b ${KAFKA_BROKER_NAME:=localhost}-t ${FORWARDER_CONFIG_TOPIC}
+kafkacat -P -b ${KAFKA_BROKER_NAME} -t ${FORWARDER_CONFIG_TOPIC}
 
-# Move the chopper to a specified speed and phase
-caput SIM:CmdS init
-caput SIM:Spd 100.0
-caput SIM:Phs 23.0
-caput SIM:CmdS start
+# TODO Move the chopper to a specified speed and phase
